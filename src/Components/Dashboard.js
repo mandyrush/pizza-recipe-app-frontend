b817fd148445b0230e9e47beb35bc9b6f16552b7
mandyrush <mandyrush85@gmail.com>
2021-12-29 22:14:50 -0700

    add delete project functionality

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -5,9 +5,25 @@ import Projects from './Projects';
 // import styles from './Dashboard.module.css';
 
 let FETCH_ALL_PROJECTS_API = 'https://pizza-recipe-app.herokuapp.com/projects';
+const DELETE_PROJECT_API = 'https://pizza-recipe-app.herokuapp.com/projects/';
 
 const Dashboard = () => {
     const [projects, setProjects] = useState([]);
+
+    const removeProject = (id) => {
+        fetch(DELETE_PROJECT_API + id, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+            .then(data => {
+                let updatedProjects = projects.filter(project => project.id !== id);
+                setProjects(updatedProjects);
+                console.log('Updated Projects: ', updatedProjects);
+            })
+            .catch(error => console.log('Error: ', error))
+    }
 
     useEffect(() => {
         fetch(FETCH_ALL_PROJECTS_API)
@@ -18,7 +34,7 @@ const Dashboard = () => {
 
     return (
         <div>
-            <Projects projects={projects} />
+            <Projects projects={projects} removeProject={removeProject} />
             <Link to="/project/create">New Project</Link>
         </div>
     );
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styles from './Projects.module.css';
 
-const Projects = ({ projects }) => {
+const Projects = ({ projects, removeProject }) => {
     return (
         <div>
             <h2>My Pizzas</h2>
@@ -11,6 +11,8 @@ const Projects = ({ projects }) => {
                     return (
                         <div key={index} className={styles.card}>
                             <Link className={styles.btn} to={`/project/${project.id}`} >{project.name}</Link>
+                            {/* <button className={styles.btn} onClick={handleEdit}>Edit</button> */}
+                            <button className={styles.btn} onClick={() => removeProject(project.id)}>Delete</button>
                         </div>
                     )
                 })}
